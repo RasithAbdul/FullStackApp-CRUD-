f1fb38bad805f0ff0fa29426b69e9c2de746c257
RasithAbdul <123852033+RasithAbdul@users.noreply.github.com>
2024-08-25 20:02:26 +0530

    Update Todo.js

--- client/client/src/Todo.js
+++ client/client/src/Todo.js
@@ -12,7 +12,7 @@ export default function Todo() {
     const [editTitle, setEditTitle] = useState("");
     const [editDescription, setEditDesciption] = useState("");
 
-    const apiUrl = "http://localhost:8000"
+    const apiUrl = "https://fullstackapp-crud-backend.onrender.com"
     
     const handleSubmit = () => {
         setError("")
